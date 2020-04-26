a10c87fb756d912564ff5b18e4269bac6e8a26d0
“““ <1790327727@qq.com>
2020-04-26 23:36:00 +0800

    UI

---
+++ test3/AlertDialog/app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.alertdialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.app.AlertDialog;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView show;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        show = findViewById(R.id.show);
+    }
+    public void customView(View source)
+    {
+        LinearLayout login = (LinearLayout) getLayoutInflater().inflate(R.layout.login, null);
+        new AlertDialog.Builder(this)
+                .setView(login)
+                .setPositiveButton("Sign in", (dialog, which) -> {show.setText("登陆");})
+
+                .setNegativeButton("Cancel", (dialog, which) -> {show.setText("取消");})
+
+                .create().show();
+    }
+}
